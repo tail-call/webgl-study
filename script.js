4f0f7a4c4beb1a85fda50e1c61f7e693c27ed704
Anton Istomin <anton@tail-call.ru>
2020-10-10 23:29:09 +0700

    i write code

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function isPowerOf2(x) {
+    return x & (x - 1) === 0;
+}
+
 class Screen {
     constructor({ parentElement, width, height }) {
         const canvas = document.createElement('canvas');
@@ -45,7 +49,7 @@ varying lowp vec4 vColor;
 
 void main() {
     gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-    vColor = aVertexColor;
+    vColor = aVertexColor * gl_Position;
 }
 `;
     }
@@ -92,6 +96,46 @@ void main() {
         }
 
         return program;
+    }
+
+    loadTexture(url) {
+        const gl = this.gl;
+
+        const texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+
+        const mipmapLevel = 0;
+        const internalFormat = gl.RGBA;
+        const width = 1;
+        const height = 1;
+        const border = 0;
+        const srcFormat = gl.RGBA;
+        const srcType = gl.UNSIGNED_BYTE;
+        const pixel = new Uint8Array([0, 0, 255, 255]);
+        gl.texImage2D(gl.TEXTURE_2D, mipmapLevel, internalFormat, width, height, border, srcFormat, srcType, pixel);
+
+        const image = new Image();
+        image.onload = () => {
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.texImage2D(gl.TEXTURE_2D, mipmapLevel, internalFormat, srcFormat, srcType, image);
+
+            // WebGL1 has different requirements for power of 2 images
+            // vs non power of 2 images so check if the image is a
+            // power of 2 in both dimensions.
+            if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+                // Yes, it's a power of 2. Generate mips.
+                gl.generateMipmap(gl.TEXTURE_2D);
+            } else {
+                // No, it's not a power of 2. Turn off mips and set
+                // wrapping to clamp to edge
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            }
+        };
+
+        image.src = url;
+        return texture;
     }
 
     addModel({ vertices, colors }) {
